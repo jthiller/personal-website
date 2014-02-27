ea160f85d67b0423feda57e870ff5661b61d50e4
Joey Hiller <jthiller@gmail.com>
2014-02-26 21:13:41 -0800

    Templating Experimentation

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,42 @@
 
 /*
- * GET home page.
- */
+* GET home page.
+*/
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Joey Hiller' });
+	res.render('index', 
+		{
+			"title": "Joey Hiller",
+			"name": "Letsgo.io",
+			"description": "Your standard form button.",
+			"state": [
+				{ 
+					"name": ":hover",
+					"escaped": "pseudo-class-hover",
+					"description": "Highlights when hovering."
+				},
+				{
+					"name": ":disabled",
+					"escaped": "pseudo-class-disabled",
+					"description": "Dims the button when disabled."
+				},
+				{
+					"name": ".primary",
+					"escaped": "primary",
+					"description": "Indicates button is the primary action."
+				},
+				{
+					"name": ".smaller",
+					"escaped": "smaller",
+					"description": "A smaller button"
+				}
+			],
+			"markup": {
+				"example": "<button>This is a button</button>",
+				"escaped": "&lt;button&gt;This is a button&lt;/button&gt;"
+			}
+		}
+	);
 };
 
 // exports.about = function(req, res){
