95118339d5521efe018957664884bf8616d3bfef
Joey Hiller <jthiller@gmail.com>
2014-03-05 16:59:49 -0800

    Cleanup

--- routes/projects.js
+++ routes/projects.js
@@ -5,10 +5,8 @@ exports.portfolio = function(req, res){
 			"title": "Portfolio<br>Redesign",
 			// "backgroundgraphic": "/images/JAPAN_Simplified.svg",
 			"projectcontent": [
-				// {
-				// 	"headline": "Creating trips around great photos."
-				// },
 				{
+
 					"sectiontitle": "Redesign 2014",
 					"content": "In building my new portfolio..."
 				},
@@ -21,9 +19,10 @@ exports.portfolio = function(req, res){
 					"image": "/images/project/portfolio/header_3d_900.jpg",
 					"imagealt": "Layered sketch of page header."
 				},
-				{
-					"content": "<p data-height='300' data-theme-id='0' data-slug-hash='kraIu' data-default-tab='result' class='codepen'>See the Pen <a href='http://codepen.io/jthiller/pen/kraIu'>Animated ☰ to × Button</a> by Joey Hiller (<a href='http://codepen.io/jthiller'>@jthiller</a>) on <a href='http://codepen.io'>CodePen</a>.</p><script async src='//codepen.io/assets/embed/ei.js'></script>"
-				},
+				// {
+				// 	"type": "iframeContent",
+				// 	"content": "<p data-height='300' data-theme-id='0' data-slug-hash='kraIu' data-default-tab='result' class='codepen'>See the Pen <a href='http://codepen.io/jthiller/pen/kraIu'>Animated ☰ to × Button</a> by Joey Hiller (<a href='http://codepen.io/jthiller'>@jthiller</a>) on <a href='http://codepen.io'>CodePen</a>.</p><script async src='//codepen.io/assets/embed/ei.js'></script>"
+				// },
 				{
 					"content": "The technical details of this site."
 				}
@@ -39,9 +38,6 @@ exports.letsgoio = function(req, res){
 			"title": "LetsGo.io",
 			// "backgroundgraphic": "/images/JAPAN_Simplified.svg",
 			"projectcontent": [
-				// {
-				// 	"headline": "Creating trips around great photos."
-				// },
 				{
 					// "sectiontitle": "",
 					"content": "In June 2013 I attended Photo Hack Day 4 with one thought in mind. &#8220;<em>I won second at PHD3. It might be a little crazy, but I know I can do better.</em>&#8221;"
